bf09c14f0a84e26b0f9b76f8392e4952e02970fa
MikkoJaakola <mikko.jaakola@hotmail.fi>
2023-04-12 17:27:13 +0300

    imported selenium and simple program to open the web page

--- CheckWebsite.py
+++ CheckWebsite.py
@@ -1,1 +1,15 @@
-print("hello world")
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+url="https://toas.fi/nopeasti-saatavilla/"
+
+driver = Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get(url)
+
+
+
+
+
