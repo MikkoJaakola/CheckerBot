de3e026177ee30325762592d703983483f61a555
Mikko Jaakola <mikko.jaakola@valagroup.com>
2023-04-14 13:39:39 +0300

    Function to get the timestamp of modification date and time

--- CheckWebsite.py
+++ CheckWebsite.py
@@ -9,14 +9,19 @@ url="https://toas.fi/nopeasti-saatavilla/"
 
 driver = Chrome(service=Service(ChromeDriverManager().install()))
 
+# Open toas website
 driver.get(url)
 
 time.sleep(5)
 
-# driver.find_element_by_css_selector('CybotCookiebotDialogBodyButton').click()
+# click cookie consent button
 driver.find_element("xpath", '//button[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
 
+modific_time = driver.find_element("xpath", '//meta[@property="article:modified_time"]').get_attribute("content")
+
 time.sleep(5)
+
+print(modific_time)
 
 
 
