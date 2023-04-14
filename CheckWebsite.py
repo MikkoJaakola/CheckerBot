e2e5aac5c1820af0d1bf7aa6a791a1d766012ba3
Mikko Jaakola <mikko.jaakola@valagroup.com>
2023-04-14 13:18:29 +0300

    Added action to click the cookie banner

--- CheckWebsite.py
+++ CheckWebsite.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 
 
 url="https://toas.fi/nopeasti-saatavilla/"
@@ -8,6 +10,13 @@ url="https://toas.fi/nopeasti-saatavilla/"
 driver = Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get(url)
+
+time.sleep(5)
+
+# driver.find_element_by_css_selector('CybotCookiebotDialogBodyButton').click()
+driver.find_element("xpath", '//button[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
+
+time.sleep(5)
 
 
 
