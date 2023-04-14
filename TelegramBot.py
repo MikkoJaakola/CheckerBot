02075d489e60df69049b6c4d2b9ea3afe469059f
Mikko Jaakola <mikko.jaakola@valagroup.com>
2023-04-14 15:32:12 +0300

    Created command for fetching modification date in telegram

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,6 +1,7 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from CheckWebsite import getModifiedTime
 
 # INSTALL pip install python-telegram-bot --upgrade
 
@@ -9,13 +10,22 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+time = getModifiedTime()
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Moro moro")
+
+async def date(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=time)
+
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token('').build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    date_handler = CommandHandler('date', date)
+    application.add_handler(date_handler)
     
     application.run_polling()
